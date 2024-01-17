d6613493e334caae125499ba3c37dd3ae5645786
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-18 02:11:40 +0700

    ok

--- bomber.py
+++ bomber.py
@@ -106,7 +106,7 @@ async def get_suggestions_for_query_async(query,country):
         return suggestions
 
 def get_suggestions_for_query(query):
-    response = requests.get(f"https://google.com/complete/search?output=toolbar&q={query}")
+    response = requests.get(f"https://google.com.vn/complete/search?output=toolbar&gl={country}&hl=vi&q={query}")
     suggestions = []
     try:
         if response.status_code == 200:
