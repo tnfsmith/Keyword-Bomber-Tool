e961d1494b54b76ca9f821def7d2b3920cba8ef1
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-18 13:09:11 +0700

    ok

--- app_ui.py
+++ app_ui.py
@@ -44,12 +44,16 @@ if st.button("Fetch Data"):
     with st.spinner("Fetching data..."):
         
         result = run_asyncio_code(input_keyword, selected_country, API_KEY) #input_country
-        if result.get('success'):
+        if result.get('success','message'):
             display_keyword_data(result['result']['keyword_data'])
             display_ai_report(result['result']['ai_report'])
         else:
             st.error("Failed to fetch data")
-
+result = {
+        
+        st.result.get ("message"),
+        
+        },
     
 # Check if the keyword has changed
 #if input_keyword != st.session_state['last_keyword']:
