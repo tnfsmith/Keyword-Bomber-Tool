fed80e42f6ef98271ab230549fb5404b43781a60
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-19 13:02:13 +0700

    ok

--- app_ui.py
+++ app_ui.py
@@ -69,11 +69,11 @@ st.markdown("""
         <h10 style="text-align: center; position: fixed; bottom: 3rem;">Developed <a href='https://lequocthai.com'>Lê Quốc Thái</a> | Contact email: <a href='mailto:lequocthai@gmail.com'>lequocthai[at]gmail.com</a> | Connect social <a href='https://t.me/tnfsmith'>Telegram</a> | <a href='tel:0985010707'>Zalo</a> </h10>""",
         unsafe_allow_html=True)
 button = """<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="LeQuocThaiy" data-color="#FFDD00" data-emoji="📖" data-font="Cookie" data-text="Buy me a Coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>"""
-html(button, height=70, width=220)
+html(button, height=60, width=200)
 st.markdown(
         """
         <style>
-            iframe[width="220"] {
+            iframe[width="200"] {
                 position: fixed;
                 bottom: 60px;
                 right: 40px;
